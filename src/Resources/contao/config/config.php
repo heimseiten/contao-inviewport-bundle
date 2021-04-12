8962ccd87711bd8fddfa3bde344e520bc1e522c9
Niels <info@heimseiten.de>
2021-04-12 19:53:24 +0200

    animation added

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,11 +1,3 @@
 <?php
 
-/*
- * This file is part of inViewport.
- * 
- * (c) Niels Hegmans 2020 <info@heimseiten.de>
- * @license GPL-3.0-or-later
- * For the full copyright and license information,
- * please view the LICENSE file that was distributed with this source code.
- * @link https://github.com/heimseiten/contao-inviewport-bundle
- */
+$GLOBALS['TL_CSS'][] = 'bundles/heimseitencontaoinviewport/inviewport.scss|static';
