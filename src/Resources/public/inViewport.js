d96185fe18f441e9fcd1c083d24091a11f64760c
Niels <info@heimseiten.de>
2022-11-03 10:00:29 +0100

    Update inViewport.js

--- src/Resources/public/inViewport.js
+++ src/Resources/public/inViewport.js
@@ -1,26 +1,24 @@
-document.addEventListener("DOMContentLoaded", function(event) {   
+document.querySelector('body').classList.add('ivp_active')
 
-  document.querySelector('body').classList.add('ivp_active')
+function inViewport(elem, margin_top, margin_bottom, callback) {
+  return new IntersectionObserver(
+    entries => {
+    entries.forEach(entry => callback(entry))
+    }, 
+    { rootMargin: margin_bottom + ' 0px ' + margin_top + ' 0px' }
+  ).observe(elem)
+}
 
-  function inViewport(elem, margin_top, margin_bottom, callback) {
-    return new IntersectionObserver(
-      entries => {
-        entries.forEach(entry => callback(entry))
-      }, 
-      { rootMargin: margin_bottom + ' 0px ' + margin_top + ' 0px' }
-    ).observe(elem)
-  }
+function changeClass(element) {
+  inViewport(element, '0px', '0px', callbackElement => {
+    if (callbackElement.isIntersecting) { element.classList.add('wivp','iivp') } else { element.classList.remove('iivp') }
+  })
+  inViewport(element, '-50%', '-50%', callbackElement => {
+     if (callbackElement.isIntersecting) { 
+       element.classList.add('over_half_ivp') } else { 
+       element.classList.remove('over_half_ivp') 
+     }
+  })
+}
 
-  function changeClass(element) {
-    inViewport(element, '0px', '0px', callbackElement => {
-      if (callbackElement.isIntersecting) { element.classList.add('wivp','iivp') } else { element.classList.remove('iivp') }
-    })
-    inViewport(element, '-50%', '-50%', callbackElement => {
-      if (callbackElement.isIntersecting) { element.classList.add('over_half_ivp') } else { element.classList.remove('over_half_ivp') 
-      }
-    })
-  }
-
-  document.querySelectorAll('.ivp').forEach(element => { changeClass(element) })
-
-})
+document.querySelectorAll('.ivp').forEach(element => { changeClass(element) })
