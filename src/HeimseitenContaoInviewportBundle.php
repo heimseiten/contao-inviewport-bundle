92269bc264b83cbd5030c22d5606c28660d2d6a0
Niels <info@heimseiten.de>
2023-04-08 14:08:57 +0200

    implement new file structur

--- src/HeimseitenContaoInviewportBundle.php
+++ src/HeimseitenContaoInviewportBundle.php
@@ -14,11 +14,22 @@ declare(strict_types=1);
 
 namespace Heimseiten\ContaoInviewportBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-/**
- * Class HeimseitenContaoInviewportBundle
- */
 class HeimseitenContaoInviewportBundle extends Bundle
 {
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+    }
 }
